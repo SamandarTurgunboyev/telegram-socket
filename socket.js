2326d928bf1bfc8b5ab83e55e2f9929b5f2d7d44
Samandar Turg'unboev <turgunboyevsamandar4@gmail.com>
2025-05-15 15:18:28 +0500

    first commit

---
+++ socket.js
@@ -0,0 +1,75 @@
+const io = require('socket.io')(5000, {
+    cors: { origin: "*", methods: ["GET", "POST"] }
+})
+
+let users = []
+
+const addOnlineUser = (user, socket) => {
+    const checkUser = users.find(u => u.user._id === user._id)
+    if (!checkUser) {
+        users.push({ user, socket })
+    }
+}
+
+const getSocketId = userId => {
+    const user = users.find(u => u.user._id === userId)
+    return user ? user.socket : null
+}
+
+io.on('connection', socket => {
+    console.log("User connected", socket.id);
+
+    socket.on("addOnlineUser", user => {
+        console.log("User added", user);
+        addOnlineUser(user, socket.id)
+        io.emit("getOnlineUser", users)
+    })
+
+    socket.on("createContact", ({ currentUser, receiver }) => {
+        const receiverSocketId = getSocketId(receiver._id)
+        console.log(receiverSocketId);
+        if (receiverSocketId) {
+            socket.to(receiverSocketId).emit('getCreatedUser', currentUser)
+        }
+    })
+
+    socket.on("newMessage", ({ newMessage, receiver, sender }) => {
+        const receiverSocketId = getSocketId(receiver._id)
+        if (receiverSocketId) {
+            socket.to(receiverSocketId).emit("getNewMessage", { newMessage, sender, receiver })
+        }
+    })
+
+    socket.on("readMessage", ({ receiver, messages }) => {
+        const receiverSocketId = getSocketId(receiver._id)
+        if (receiverSocketId) {
+            socket.to(receiverSocketId).emit("getReadMessage", messages)
+        }
+    })
+
+    socket.on("updateMessage", ({ updateMessage, receiver, sender }) => {
+        const receiverId = getSocketId(receiver._id)
+        if (receiverId) {
+            socket.to(receiverId).emit("getUpdateMessage", { updateMessage, receiver, sender })
+        }
+    })
+
+    socket.on("deleteMessage", ({ deleteMessage, receiver, sender, filteredMsg }) => {
+        const receiverId = getSocketId(receiver._id)
+        if (receiverId) {
+            socket.to(receiverId).emit("getDeleteMessage", { deleteMessage, receiver, sender, filteredMsg })
+        }
+    })
+
+    socket.on("typing", ({ receiver, sender, message }) => {
+        const receiverId = getSocketId(receiver._id)
+        if (receiverId) {
+            socket.to(receiverId).emit("getTyping", { sender, message })
+        }
+    })
+
+    socket.on('disconnect', () => {
+        users = users.filter(u => u.socket !== socket.id)
+        io.emit("getOnlineUser", users)
+    })
+})
