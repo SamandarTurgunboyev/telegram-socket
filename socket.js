22eb2abfa32135d4deea3ae81e49a8e6b6dfb982
Samandar Turg'unboev <turgunboyevsamandar4@gmail.com>
2025-05-15 17:41:48 +0500

    PORT

--- socket.js
+++ socket.js
@@ -1,6 +1,22 @@
-const io = require('socket.io')(5000, {
-    cors: { origin: "*", methods: ["GET", "POST"] }
-})
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    },
+    transports: ['websocket'],
+});
+
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+    console.log(`Server ${PORT}-portda ishlamoqda`);
+});
 
 let users = []
 
